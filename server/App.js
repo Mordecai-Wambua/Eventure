bf422d61aa77ae6c3e6c632f46248f67b6e5c338
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-10-06 13:38:05 +0300

    Created the database Schemas and made minor upgrades to files

--- server/App.js
+++ server/App.js
@@ -1,16 +1,20 @@
 // Main entry point
-const express = require('express');
-const database = require('./config/db');
+import express from 'express';
+import router from './routes/router.js';
+import database from './config/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use('/', router);
+
 // Connect to MongoDB
 (async () => {
-  await database.connect()
-    const testCollection = database.connection.connection.db.collection('test');
-    await testCollection.insertOne({ test: 'data' });
-    console.log('Inserted test document');
+  await database.connect();
+  // const testCollection = database.connection.connection.db.collection('test');
+  // await testCollection.insertOne({ test: 'data' });
+  // console.log('Inserted test document');
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

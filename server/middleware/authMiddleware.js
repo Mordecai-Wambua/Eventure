095ce9fbcf82e9b30a2111a801778a47057f7e3e
Lumasia <lumasiastancelaus@gmail.com>
2024-10-06 18:32:56 +0300

    First version of Auth with JWT

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+// Verify generated JWT
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_KEY;
+
+export const authJWT = (req, res, next) => {
+  const token = req.header('Authorization')?.split(' ')[1]; // Assuming the token is sent as 'Bearer <token>'
+  
+  if (!token) {
+    return res.status(401).json({ message: 'Access denied. No token provided.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid token' });
+  }
+};
